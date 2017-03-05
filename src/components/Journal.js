bcd44c5c1c77d1466a9f285a67a30928a1934f5b
Loger <vladisl.mukhin@gmail.com>
2017-03-05 16:57:08 +0300

    change form group for add operation

--- src/components/Journal.js
+++ src/components/Journal.js
@@ -3,11 +3,31 @@ import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import classNames from 'classnames'
 import { selectRow, ActionAddOperation } from '../actions'
+import './Journal.scss'
 
 class Journal extends Component {
     constructor(props) {
         super(props)
+        this.state = {
+            toggledButton: true,
+            op_type: '+'
+        }
     }
+
+    toggleButtonOff() {
+        this.setState(() => ({
+            toggledButton: false,
+            op_type: '-'
+        }));
+    }
+    toggleButtonOn() {
+        this.setState(() => ({
+            toggledButton: true,
+            op_type: '+'
+        }));
+    }
+
+
     tableList() {
         return (
             this.props.journal.map((table,id) => {
@@ -27,12 +47,13 @@ class Journal extends Component {
     }
     addOperation() {
         let payload = {
-            op_type: '+',
+            op_type: this.state.op_type,
             sum: this.textInputSum.value,
-            kat: 'test',
-            comment: 'test comment'
+            kat: this.textInputKat.value,
+            comment: this.textInputComment.value
         }
-        this.textInputSum.value='';
+        this.textInputSum.value = 0;
+        this.textInputKat.value = this.textInputComment.value = '';
         return this.props.addOperation(payload)
     }
     render() {
@@ -56,10 +77,37 @@ class Journal extends Component {
                                         <h4 className="modal-title" id="myModalLabel">Добавление операции</h4>
                                     </div>
                                     <div className="modal-body">
-                                        <div className="input-group">
-                                            <span className="input-group-addon">BYN</span>
-                                            <input type="number" ref={(inputSum) => { this.textInputSum = inputSum; }} className="form-control"/>
+                                        <form>
+                                        <div className="form-group">
+                                            {/*toggle_start*/}
+                                            <div className="btn-group" data-toggle="buttons">
+                                                {/*<label onClick={::this.toggleButtonOn} className="btn btn-info active">*/}
+                                                    {/*<input type="radio" />Доход*/}
+                                                {/*</label>*/}
+                                                {/*<label onClick={::this.toggleButtonOff} className="btn btn-info">*/}
+                                                    {/*<input type="radio" />Расход*/}
+                                                {/*</label>*/}
+                                                <button onClick={::this.toggleButtonOn} className={classNames('btn btn-info', {active: this.state.toggledButton})}>Доход</button>
+                                                <button onClick={::this.toggleButtonOff} className={classNames('btn btn-info', {active: !this.state.toggledButton})}>Расход</button>
+                                            </div>
+                                            {/*toggle_end*/}
                                         </div>
+                                        <div className="form-group">
+                                            <label htmlFor="inputSum">Сумма</label>
+                                            <div className="input-group" id="inputSum">
+                                                <span className="input-group-addon">BYN</span>
+                                                <input type="number" defaultValue='0' ref={(inputSum) => { this.textInputSum = inputSum; }} className="form-control"/>
+                                            </div>
+                                        </div>
+                                        <div className="form-group">
+                                            <label htmlFor="inputKat">Категория</label>
+                                            <input id="inputKat" type="text" placeholder="Введите категорию" ref={(inputKat) => { this.textInputKat = inputKat; }} className="form-control"/>
+                                        </div>
+                                        <div className="form-group">
+                                            <label htmlFor="inputComment">Комментарий</label>
+                                            <textarea rows="3" placeholder="Введите комментарий" id="inputComment" ref={(inputComment) => { this.textInputComment = inputComment; }} className="form-control"/>
+                                        </div>
+                                        </form>
                                     </div>
                                     <div className="modal-footer">
                                         <button type="button" className="btn btn-default" data-dismiss="modal">Закрыть</button>
