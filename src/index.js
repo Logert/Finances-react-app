7371eb224c5a87f1ff1c5db98cf713e531b17203
Loger <vladisl.mukhin@gmail.com>
2017-03-05 20:41:43 +0300

    add react-router-redux

--- src/index.js
+++ src/index.js
@@ -6,12 +6,15 @@ import { routes } from './routes'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import rootReducer from './reducers'
+import { syncHistoryWithStore } from 'react-router-redux'
 
 const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
+const history = syncHistoryWithStore(browserHistory, store)
+
 render(
     <Provider store={store}>
-        <Router history={browserHistory} routes={routes}/>
+        <Router history={history} routes={routes}/>
     </Provider>
     ,
     document.getElementById('root')
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import JournalReducer from './Journal-reducer';
 import MoneyReducer from './Money-reducer'
+import { routerReducer } from 'react-router-redux'
 
 const rootReducer = combineReducers({
     JournalReducer,
-    MoneyReducer
+    MoneyReducer,
+    routing: routerReducer
 });
 
 export default rootReducer;
