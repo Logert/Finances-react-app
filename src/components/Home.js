5c88b7c6c521f78754df16ac02c3f5c75a56adb6
Loger <vladisl.mukhin@gmail.com>
2017-03-07 13:32:08 +0300

    add jimbotron

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router'
 import './Home.scss'
 
 export default class Home extends Component {
     render() {
         return (
-            <div>
+            <div className="jumbotron">
                 <h1>Главная</h1>
-                <div className="Home"></div>
+                <p>Объедините все ваши финансы в одной системе. Воспользуйтесь возможностями домашней онлайн-бухгалтерии для учета, анализа и планирования расходов - эффективных способов держать свою финансовую жизнь под контролем!</p>
+                <p><Link className="btn btn-primary btn-lg" role="button" to="/journal">Перейти в журнал</Link></p>
             </div>
-
         )
     }
 }
